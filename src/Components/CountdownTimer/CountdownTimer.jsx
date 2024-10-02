d469207604c00e819d11adfb1ab5a56a532af599
tranchi0120 <tranchi20012000@gmail.com>
2024-10-02 09:51:03 +0700

    done countdown and listProduct render UI

---
+++ src/Components/CountdownTimer/CountdownTimer.jsx
@@ -0,0 +1,64 @@
+import Button from '@components/Button/Button.jsx'
+import style from './style.module.scss'
+import { useState, useEffect } from 'react'
+
+const CountdownTimer = () => {
+  const [dataCountDown, setDataCountDown] = useState({
+    days: 30,
+    hours: 10,
+    minutes: 50,
+    seconds: 60
+  })
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setDataCountDown((prev) => {
+        const { days, hours, minutes, seconds } = prev
+        switch (true) {
+          case seconds > 0:
+            return { ...prev, seconds: seconds - 1 }
+          case minutes > 0:
+            return { ...prev, minutes: minutes - 1, seconds: 59 }
+          case hours > 0:
+            return { ...prev, hours: hours - 1, minutes: 59, seconds: 59 }
+          case days > 0:
+            return {
+              ...prev,
+              days: days - 1,
+              hours: 59,
+              minutes: 59,
+              seconds: 59
+            }
+          default:
+        }
+        clearTimeout(countdown)
+        return prev
+      })
+    }, [1000])
+    return () => clearInterval(countdown)
+  }, [])
+
+  return (
+    <>
+      <div className={style.countdown_main}>
+        <div className={style.countdown_time}>
+          <div className={style.countdown_reduce}>
+            {dataCountDown.days} <span>Days</span>
+          </div>
+          <div className={style.countdown_reduce}>
+            {dataCountDown.hours} <span>Hours</span>
+          </div>
+          <div className={style.countdown_reduce}>
+            {dataCountDown.minutes} <span>Min</span>
+          </div>
+          <div className={style.countdown_reduce}>
+            {dataCountDown.seconds} <span>Secs</span>
+          </div>
+        </div>
+        <p className={style.countdown_title}>The classics make a comeback</p>
+        <Button content={'Buy Now'} />
+      </div>
+    </>
+  )
+}
+export default CountdownTimer
--- src/Components/Product/toastBox/ToastBox.jsx
+++ src/Components/Product/toastBox/ToastBox.jsx
@@ -1,34 +1,32 @@
 import shoppingCartIcon from '@icons/svgs/shopingCartIcon.svg'
-import heartIcon  from '@icons/svgs/heartIcon.svg'
+import heartIcon from '@icons/svgs/heartIcon.svg'
 import reloadIcon from '@icons/svgs/reloadIcon.svg'
 import eyeIcon from '@icons/svgs/eyeIcon.svg'
 import style from '../style.module.scss'
 
-const ToastBox = ({type, href}) => {
-
-    const onChangeIcon = type => {
-        switch (type) {
-            case 'shoppingCartIcon':
-                return shoppingCartIcon
-            case 'heartIcon':
-                return heartIcon
-            case 'reloadIcon':
-                return reloadIcon
-            case 'eyeIcon':
-                return eyeIcon
-            default:
-                return null
-        }
+const ToastBox = ({ type, href }) => {
+  const onChangeIcon = (type) => {
+    switch (type) {
+      case 'shoppingCartIcon':
+        return shoppingCartIcon
+      case 'heartIcon':
+        return heartIcon
+      case 'reloadIcon':
+        return reloadIcon
+      case 'eyeIcon':
+        return eyeIcon
+      default:
+        return null
     }
+  }
 
-    return (
-        <div className={style.boxIcon}>
-            <a href={href} >
-                <img src={onChangeIcon(type)} alt={href} />
-            </a>
-        </div>
-    )
+  return (
+    <div className={style.boxIcon}>
+      <a href={href}>
+        <img src={onChangeIcon(type)} alt={href} />
+      </a>
+    </div>
+  )
 }
 
-
-export default ToastBox;
+export default ToastBox
