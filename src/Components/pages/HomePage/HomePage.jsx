2725245e7bb30b4ed6d05ea9ffd771d69f9ed0b3
tranchi0120 <tranchi20012000@gmail.com>
2024-10-02 10:23:59 +0700

    done footer

--- src/Components/pages/HomePage/HomePage.jsx
+++ src/Components/pages/HomePage/HomePage.jsx
@@ -1,17 +1,19 @@
 import Banner from '@components/Banner/Banner'
 import Info from '@components/Info/Info'
 import Header from '@components/Header/Header'
-import Product from "@components/Product/Product.jsx";
+import Product from '@components/Product/Product.jsx'
+import Footer from '@components/Footer/Footer.jsx'
 
 const HomePage = () => {
-    return (
-        <div>
-            <Header />
-            <Banner />
-            <Info />
-            <Product/>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Info />
+      <Product />
+      <Footer />
+    </div>
+  )
 }
 
 export default HomePage
